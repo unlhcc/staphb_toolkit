f3ea53f237163ef0a2559585b5df9064acee9d10
Adam Caprez <acaprez2@unl.edu>
2020-03-04 04:56:09 +0000

    Create proper packaging.

--- setup.py
+++ setup.py
@@ -1,59 +1,23 @@
-#!/usr/bin/env python3
-import subprocess, os, sys
+from setuptools import setup, find_packages
 
-print("Checking Dependencies...")
+setup(
+    name="staphb_toolkit",
+    version="1.0",
+    url="https://staph-b.github.io/staphb_toolkit/",
+    description="Python Toolkit for using containerized programs in either Singularity or Docker",
+    packages=["staphb_toolkit.core","staphb_toolkit.lib","staphb_toolkit.workflows"],
+    package_dir={"staphb_toolkit.core": "core", "staphb_toolkit.lib": "lib", "staphb_toolkit.workflows": "workflows" },
+    license="GPLv3",
+    scripts = [
+        "staphb_toolkit",
+        "staphb_toolkit_workflows"
+    ],
+    install_requires=[
+    "spython>=0.0.73",
+    "psutil>=5.6.3",
+    "docker>=4.1.0"
+    ],
+    python_requires='>=3.7',
+    zip_safe=False
+)
 
-toolkit_dir = os.path.dirname(os.path.abspath(__file__))
-
-python_version = ""
-pip_cmd = ""
-container = False
-#check python version
-try:
-    stdout = subprocess.run(["python3","-V"],stdout=subprocess.PIPE)
-    if "Python 3.7" in stdout.stdout.decode('utf-8'):
-        python_version = "3.7"
-    else:
-        os.system("which python3")
-        print("Check your python installation, python3 must be >= python version 3.7")
-        sys.exit(1)
-except FileNotFoundError:
-    print("Check your python installation, python3 must be >= python version 3.7")
-    sys.exit(1)
-
-#check pip version
-try:
-    stdout = subprocess.run(["pip3","-V"],stdout=subprocess.PIPE)
-    if "python 3.7" in stdout.stdout.decode('utf-8'):
-        pip_cmd = "pip3"
-except:
-    pass
-
-try:
-    stdout = subprocess.run(["pip","-V"],stdout=subprocess.PIPE)
-    if "python 3.7" in stdout.stdout.decode('utf-8'):
-        pip_cmd = "pip"
-except:
-    pass
-
-if not pip_cmd:
-    print("Please verify the python 3.7 pip installation.")
-    sys.exit(1)
-
-if python_version and pip_cmd:
-    print("Installing dependencies")
-    requirements_path = os.path.join(toolkit_dir,"requirements.txt")
-    stdout = subprocess.run([pip_cmd,"install","-r",requirements_path],stdout=subprocess.PIPE)
-    print(stdout.stdout.decode('utf-8'))
-
-#add to path for bash
-#TODO make this more useful for other shells
-print("Setting up path...")
-stdout = subprocess.run(["which","staphb_toolkit"],stdout=subprocess.PIPE)
-if not stdout.stdout.decode('utf-8'):
-    homedir = os.environ['HOME']
-    bashrc = os.path.join(homedir,".bashrc")
-    with open(bashrc,'a') as outfile:
-        outfile.write("#path for staphb_toolkit\n")
-        outfile.write(f"export PATH=\"{toolkit_dir}:$PATH\"")
-print("Done")
