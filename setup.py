79b0fe1692f529ba67ebd3e7813d0ccb312a469a
Adam Caprez <acaprez2@unl.edu>
2020-03-04 05:14:08 +0000

    Add nested packages and data files.

--- setup.py
+++ setup.py
@@ -3,11 +3,27 @@ from setuptools import setup, find_packages
 setup(
     name="staphb_toolkit",
     version="1.0",
+    license="GPLv3",
     url="https://staph-b.github.io/staphb_toolkit/",
     description="Python Toolkit for using containerized programs in either Singularity or Docker",
-    packages=["staphb_toolkit.core","staphb_toolkit.lib","staphb_toolkit.workflows"],
-    package_dir={"staphb_toolkit.core": "core", "staphb_toolkit.lib": "lib", "staphb_toolkit.workflows": "workflows" },
-    license="GPLv3",
+    packages=[
+        "staphb_toolkit.core",
+        "staphb_toolkit.lib",
+        "staphb_toolkit.workflows",
+        "staphb_toolkit.workflows.dryad",
+        "staphb_toolkit.workflows.dryad.app",
+        "staphb_toolkit.workflows.foushee",
+        "staphb_toolkit.workflows.tredegar",
+        ],
+    package_dir={
+        "staphb_toolkit.core": "core",
+        "staphb_toolkit.lib": "lib",
+        "staphb_toolkit.workflows": "workflows" },
+    package_data={
+        "staphb_toolkit.workflows.foushee": ["foushee_config.json"],
+        "staphb_toolkit.workflows.tredegar": ["tredegar_config.json"],
+        "staphb_toolkit.core": ["docker_config.json"],
+    },
     scripts = [
         "staphb_toolkit",
         "staphb_toolkit_workflows"
